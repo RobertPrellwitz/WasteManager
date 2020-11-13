036f745f65ac70af204160d8b6826dcf5741bcbc
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-11-13 11:58:00 -0600

    project creation

---
+++ GarbageCollection/GarbageCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GarbageCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
